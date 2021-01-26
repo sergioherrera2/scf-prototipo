516fac71af980391ec07f6a8c15fa6f605870b84
Fernando <fernando.fvb.fvb@gmail.com>
2021-01-26 19:57:25 +0100

    Añadida conexión MQTT + Como desplegar Grafana

--- Termostato_2/src/main.cpp
+++ Termostato_2/src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <PubSubClient.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
@@ -24,6 +27,47 @@ DHT dht(DHTPIN, DHTTYPE);
 const int fireTemperature = 50;
 const int airQualityLimit = 50;
 
+const char* ssid = "FIBRAVILLARRUBIA_7CA3";
+const char* password =  "qjgkbER6";
+
+#define TOPIC "1111111A/1/1/1"
+#define BROKER_IP "192.168.18.8"
+#define BROKER_PORT 2883
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void wifiConnect()
+{
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void mqttConnect() {
+  client.setServer(BROKER_IP, BROKER_PORT);
+  while (!client.connected()) {
+    Serial.print("MQTT connecting ...");
+
+    if (client.connect("ESP32Client1")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, status code =");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+
+      delay(5000);  //* Wait 5 seconds before retrying
+    }
+  }
+}
+
 static enum { ST_RED,
               ST_GREEN,
               ST_BLUE,
@@ -32,6 +76,7 @@ static enum { ST_RED,
 TaskHandle_t t2;
 TaskHandle_t t3;
 TaskHandle_t t5;
+TaskHandle_t t6;
 
 QueueHandle_t q2_temperature;
 QueueHandle_t q3_light;
@@ -41,6 +86,7 @@ void IRAM_ATTR on_handleInterrupt()
    vTaskResume(t2);
    vTaskResume(t3);
    vTaskResume(t5);
+   vTaskResume(t6);
 }
 
 void IRAM_ATTR off_handleInterrupt()
@@ -49,6 +95,7 @@ void IRAM_ATTR off_handleInterrupt()
    vTaskSuspend(t2);
    vTaskSuspend(t3);
    vTaskSuspend(t5);
+   vTaskSuspend(t6);
 }
 
 void control_RGB_leds(unsigned char red, unsigned char green, unsigned char blue)
@@ -160,6 +207,18 @@ void vTaskAirQuality(void *pvParam)
    vTaskDelete(NULL);
 }
 
+void vTaskSendMQTT(void *pvParam)
+{
+   const portTickType xticks = 1000 / portTICK_RATE_MS;
+   for (;;)
+   {
+      String jsonData = "{\"temperatura\":"+String(20)+",\"calidadAire\":"+String(20)+"}";
+      client.publish(TOPIC, jsonData.c_str());
+      vTaskDelay(1000 / portTICK_RATE_MS);
+   }
+   vTaskDelete(NULL);
+}
+
 void setup()
 {
    q2_temperature = xQueueCreate(5, sizeof(float));
@@ -186,12 +245,17 @@ void setup()
    xTaskCreatePinnedToCore(vTask2, "Task 2", 4000, NULL, 3, &t2, 1);
    xTaskCreatePinnedToCore(vTask3, "Task 3", 4000, NULL, 3, &t3, 1);
    xTaskCreatePinnedToCore(vTaskAirQuality, "Task Light", 4000, NULL, 3, &t5, 1);
+   xTaskCreatePinnedToCore(vTaskSendMQTT, "Task MQTT", 4000, NULL, 3, &t6, 1);
 
    vTaskSuspend(t2);
    vTaskSuspend(t3);
    vTaskSuspend(t5);
+   vTaskSuspend(t6);
 
    next_state = ST_NONE;
+
+   wifiConnect();
+   mqttConnect();
 }
 
 void loop()
