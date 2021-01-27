da241bbf660965352ae904a6e7211265dedae8fe
jorgemm7 <jorgemm777@gmail.com>
2021-01-27 18:20:56 +0100

    Humedad

--- Termostato_2/src/main.cpp
+++ Termostato_2/src/main.cpp
@@ -16,7 +16,7 @@
 #define blueLed D11
 
 #define rotation A0
-#define light A1
+#define air A1
 #define DHTPIN D4
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
@@ -27,11 +27,11 @@ DHT dht(DHTPIN, DHTTYPE);
 const int fireTemperature = 50;
 const int airQualityLimit = 50;
 
-const char* ssid = "FIBRAVILLARRUBIA_7CA3";
-const char* password =  "qjgkbER6";
+const char *ssid = "MiFibra-36E9-plus";
+const char *password = "XMe3cpbk";
 
 #define TOPIC "1111111A/1/1/1"
-#define BROKER_IP "192.168.18.8"
+#define BROKER_IP "192.168.1.13"
 #define BROKER_PORT 2883
 
 WiFiClient espClient;
@@ -39,33 +39,39 @@ PubSubClient client(espClient);
 
 void wifiConnect()
 {
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
+   WiFi.begin(ssid, password);
 
-  Serial.println("Connected to the WiFi network");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+   while (WiFi.status() != WL_CONNECTED)
+   {
+      delay(1000);
+      Serial.println("Connecting to WiFi..");
+   }
+
+   Serial.println("Connected to the WiFi network");
+   Serial.print("IP Address: ");
+   Serial.println(WiFi.localIP());
 }
 
-void mqttConnect() {
-  client.setServer(BROKER_IP, BROKER_PORT);
-  while (!client.connected()) {
-    Serial.print("MQTT connecting ...");
+void mqttConnect()
+{
+   client.setServer(BROKER_IP, BROKER_PORT);
+   while (!client.connected())
+   {
+      Serial.print("MQTT connecting ...");
 
-    if (client.connect("ESP32Client1")) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, status code =");
-      Serial.print(client.state());
-      Serial.println("try again in 5 seconds");
+      if (client.connect("ESP32Client1"))
+      {
+         Serial.println("connected");
+      }
+      else
+      {
+         Serial.print("failed, status code =");
+         Serial.print(client.state());
+         Serial.println("try again in 5 seconds");
 
-      delay(5000);  //* Wait 5 seconds before retrying
-    }
-  }
+         delay(5000); //* Wait 5 seconds before retrying
+      }
+   }
 }
 
 static enum { ST_RED,
@@ -77,9 +83,11 @@ TaskHandle_t t2;
 TaskHandle_t t3;
 TaskHandle_t t5;
 TaskHandle_t t6;
+TaskHandle_t t7;
 
+QueueHandle_t q1_humidity;
 QueueHandle_t q2_temperature;
-QueueHandle_t q3_light;
+QueueHandle_t q3_air;
 
 void IRAM_ATTR on_handleInterrupt()
 {
@@ -87,6 +95,7 @@ void IRAM_ATTR on_handleInterrupt()
    vTaskResume(t3);
    vTaskResume(t5);
    vTaskResume(t6);
+   vTaskResume(t7);
 }
 
 void IRAM_ATTR off_handleInterrupt()
@@ -96,6 +105,7 @@ void IRAM_ATTR off_handleInterrupt()
    vTaskSuspend(t3);
    vTaskSuspend(t5);
    vTaskSuspend(t6);
+   vTaskSuspend(t7);
 }
 
 void control_RGB_leds(unsigned char red, unsigned char green, unsigned char blue)
@@ -178,21 +188,23 @@ void vTask3(void *pvParam)
 void vTaskAirQuality(void *pvParam)
 {
    const portTickType xticks = 5000 / portTICK_RATE_MS;
-   float light_val = 0.0;
+   float air_val = 0.0;
 
    for (;;)
    {
-      light_val = analogRead(light);
+      air_val = analogRead(air);
       /* Transformamos valor de 0-100 */
-      light_val /= 1023;
-      light_val *= 100;
+      air_val /= 1023;
+      air_val *= 100;
 
-      //portBASE_TYPE st = xQueueSendToBack(q3_light, &light_val, xticks);
-      //if (st != pdPASS)
-      //  Serial.printf("No puedo enviar Luz\r\n");
-      Serial.printf("Valor de luz: %d\r\n", (int)light_val);
+      portBASE_TYPE st = xQueueSendToBack(q3_air, &air_val, xticks);
+      if (st != pdPASS)
+      {
+         Serial.printf("No puedo enviar Luz\r\n");
+      }
+      Serial.printf("Valor de luz: %d\r\n", (int)air_val);
 
-      if ((int)light_val > airQualityLimit)
+      if ((int)air_val > airQualityLimit)
       {
          next_state = ST_RED;
       }
@@ -201,7 +213,6 @@ void vTaskAirQuality(void *pvParam)
          next_state = ST_GREEN;
       }
 
-      //delay(500);
       vTaskDelay(5000 / portTICK_RATE_MS);
    }
    vTaskDelete(NULL);
@@ -209,21 +220,60 @@ void vTaskAirQuality(void *pvParam)
 
 void vTaskSendMQTT(void *pvParam)
 {
+   float temp_val_rec;
+   float air_val_rec;
+   float hum_val_rec;
    const portTickType xticks = 1000 / portTICK_RATE_MS;
+
    for (;;)
    {
-      String jsonData = "{\"temperatura\":"+String(20)+",\"calidadAire\":"+String(20)+"}";
-      client.publish(TOPIC, jsonData.c_str());
+      portBASE_TYPE st_hum;
+      st_hum = xQueueReceive(q1_humidity, &hum_val_rec, xticks);
+
+      portBASE_TYPE st_temp;
+      st_temp = xQueueReceive(q2_temperature, &temp_val_rec, xticks);
+
+      portBASE_TYPE st_air;
+      st_air = xQueueReceive(q3_air, &air_val_rec, xticks);
+
+      if (st_temp == pdPASS && st_air == pdPASS && st_hum == pdPASS)
+      {
+         String jsonData = "{\"temperatura\":" + (String)temp_val_rec + ",\"calidadAire\":" 
+               + (String)air_val_rec + + ",\"humedad\":" + (String)hum_val_rec + "}";
+         client.publish(TOPIC, jsonData.c_str());
+      }
       vTaskDelay(1000 / portTICK_RATE_MS);
+   }
+   vTaskDelete(NULL);
+}
+
+void vTaskHumidity(void *pvParam)
+{
+   const portTickType xticks = 250 / portTICK_RATE_MS;
+   float humidity_val = 0.0;
+
+   for (;;)
+   {
+      humidity_val = dht.readHumidity();
+      Serial.printf("Humedad: %f", humidity_val);
+
+      /*portBASE_TYPE st_hum = xQueueSendToBack(q1_humidity, &humidity_val, xticks);
+      if (st_hum != pdPASS)
+      {
+         Serial.printf("No puedo enviar humedad Actual\r\n");
+      }*/
+      
+      vTaskDelay(250 / portTICK_RATE_MS);
    }
    vTaskDelete(NULL);
 }
 
 void setup()
 {
+   q1_humidity = xQueueCreate(5, sizeof(float));
    q2_temperature = xQueueCreate(5, sizeof(float));
-   q3_light = xQueueCreate(5, sizeof(float));
-
+   q3_air = xQueueCreate(5, sizeof(float));
+   
    Serial.begin(9600);
    pinMode(A0, INPUT);
    pinMode(A1, INPUT);
@@ -244,8 +294,9 @@ void setup()
    xTaskCreatePinnedToCore(vTaskRGB, "Task RGB", 4000, NULL, 1, NULL, 1);
    xTaskCreatePinnedToCore(vTask2, "Task 2", 4000, NULL, 3, &t2, 1);
    xTaskCreatePinnedToCore(vTask3, "Task 3", 4000, NULL, 3, &t3, 1);
-   xTaskCreatePinnedToCore(vTaskAirQuality, "Task Light", 4000, NULL, 3, &t5, 1);
+   xTaskCreatePinnedToCore(vTaskAirQuality, "Task Air", 4000, NULL, 3, &t5, 1);
    xTaskCreatePinnedToCore(vTaskSendMQTT, "Task MQTT", 4000, NULL, 3, &t6, 1);
+   xTaskCreatePinnedToCore(vTaskHumidity, "Task Humidity", 4000, NULL, 3, &t7, 1);
 
    vTaskSuspend(t2);
    vTaskSuspend(t3);
